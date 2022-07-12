1f85a19450f9b9e7e9099175ded82de8723ce53d
Junjae Lee <ejj612o@gmail.com>
2022-07-12 23:00:24 +0900

    python nidaqmx 연동

---
+++ read.py
@@ -0,0 +1,22 @@
+import nidaqmx
+import time
+from time import ctime
+
+task = nidaqmx.Task()
+task.ai_channels.add_ai_voltage_chan("cDAQ1Mod1/ai0")
+task.timing.cfg_samp_clk_timing(rate=51200,
+                                active_edge=nidaqmx.constants.Edge.RISING,
+                                sample_mode=nidaqmx.constants.AcquisitionType.FINITE,
+                                samps_per_chan=51200)
+
+for i in range(0, 5):
+    start = time.time()
+    data = task.read(number_of_samples_per_channel=51200)
+    end = time.time()
+
+    print("=============================================")
+    print("data length : ", len(data))
+    print("start time", ctime(start))
+    print("end time", ctime(end))
+    print("running time (second) :", end - start)
+    print("=============================================")
