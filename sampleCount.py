ab952ff4d84c1fce44e739afacd3310ae4fcee07
Junjae Lee <ejj612o@gmail.com>
2022-10-18 14:30:34 +0900

    센서 측정 변경시도

--- sampleCount.py
+++ sampleCount.py
@@ -10,18 +10,31 @@ from time import ctime, time, localtime, strftime
 task = nidaqmx.Task()
 task.ai_channels.add_ai_voltage_chan("cDAQ1Mod1/ai0")
 
-sampleRate = 100     # Sample Rate in Hz
-secsToAcquire = 10    # Number of seconds over which to acquire data
-numberOfSamples = int(secsToAcquire * sampleRate)
+sampleRate = 2500    # Sample Rate in Hz
+numberOfSamples = 1
 
 # https://github.com/tenss/Python_DAQmx_examples
 
 task.timing.cfg_samp_clk_timing(
-    sampleRate, source='', active_edge=Edge.RISING, sample_mode=AcquisitionType.FINITE, samps_per_chan=numberOfSamples)
+    sampleRate, source='', active_edge=Edge.RISING, sample_mode=AcquisitionType.CONTINUOUS)
 
-while(True):
-    datas = task.read(
-        number_of_samples_per_channel=constants.READ_ALL_AVAILABLE)
+
+def printData(tTask, event_type, num_samples, callback_data):
+    datas = task.read(sampleRate)
     now = time()
     now_str = ctime(now)
-    print(now_str, " 센서로 부터 값 획득", len(datas))
+    # print(now_str, " 센서로 부터 값 획득", len(datas))
+    print(now_str, " 센서로 부터 값 획득")
+
+    return 0
+
+
+task.register_every_n_samples_acquired_into_buffer_event(
+    sampleRate, printData)
+
+task.start()
+
+while True:
+    pass
+
+task.stop()
